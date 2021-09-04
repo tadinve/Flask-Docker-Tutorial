2091d99cc26644d684745fabfc3dbf7d102981ae
Venkatesh Tadinada <venkat@solivar.com>
2021-09-03 22:50:20 -0700

    tab01 step 4 -containers and docker

--- manage.py
+++ manage.py
@@ -7,16 +7,19 @@ import subprocess
 
 import click
 
+docker_compose_file = "docker/development.yml"
+docker_compose_cmdline = ["docker-compose", "-f", docker_compose_file]
+
 
 # Ensure an environment variable exists and has a value
 def setenv(variable, default):
     os.environ[variable] = os.getenv(variable, default)
 
 
-setenv("APPLICATION_CONFIG", "development") 
+setenv("APPLICATION_CONFIG", "development")
 
 # Read configuration from the relative JSON file
-config_json_filename = os.getenv("APPLICATION_CONFIG") + ".json" 
+config_json_filename = os.getenv("APPLICATION_CONFIG") + ".json"
 with open(os.path.join("config", config_json_filename)) as f:
     config = json.load(f)
 
@@ -34,7 +37,7 @@ def cli():
 
 @cli.command(context_settings={"ignore_unknown_options": True})
 @click.argument("subcommand", nargs=-1, type=click.Path())
-def flask(subcommand): 
+def flask(subcommand):
     cmdline = ["flask"] + list(subcommand)
 
     try:
@@ -45,7 +48,18 @@ def flask(subcommand):
         p.wait()
 
 
-cli.add_command(flask)
+@cli.command(context_settings={"ignore_unknown_options": True})
+@click.argument("subcommand", nargs=-1, type=click.Path())
+def compose(subcommand):
+    cmdline = docker_compose_cmdline + list(subcommand)
+
+    try:
+        p = subprocess.Popen(cmdline)
+        p.wait()
+    except KeyboardInterrupt:
+        p.send_signal(signal.SIGINT)
+        p.wait()
+
 
 if __name__ == "__main__":
     cli()
