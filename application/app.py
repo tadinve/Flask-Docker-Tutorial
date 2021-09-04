96c5c4105ac1a36384e349294e170c4c9181a5a7
Venkatesh Tadinada <venkat@solivar.com>
2021-09-04 00:42:14 -0700

    finished tab 2

--- application/app.py
+++ application/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from application.models import User
 
 def create_app(config_name): 
 
@@ -8,8 +9,19 @@ def create_app(config_name):
 
     app.config.from_object(config_module)
 
+    from application.models import db, migrate
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     @app.route("/") 
     def hello_world():
         return "Hello, World!"
+
+    @app.route("/users")
+    def users():
+        num_users = User.query.count()
+        return f"Number of users: {num_users}"
+
 
     return app
